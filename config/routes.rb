f92d876df873ab308bb129ab9df1636290a24ecb
vjdodge <valoriejones@gmail.com>
2016-05-22 11:46:19 -0700

    Welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/about'
-
-  get 'welcome/index'
+  get 'about' => 'welcome#about'
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
